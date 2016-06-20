c5478345734a06501d20ce80054617ee4ab7749a
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2016-06-20 12:45:20 +0200

    Fix issue with hero component

--- NightClazz/src/app/hero/hero.component.ts
+++ NightClazz/src/app/hero/hero.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
   moduleId: module.id,
   selector: 'hero',
   template: `
-    {{hero}}
+    {{name}}
   `,
   styleUrls: ['hero.component.css']
 })
